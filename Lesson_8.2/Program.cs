6194128021a963df7b054e19c832b2867f9a71e5
Александр Захаров <zaher0861@yandex.ru>
2021-12-14 00:34:19 +0300

    Добавьте файлы проекта.

---
+++ Lesson_8.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Создаем файл и вносим в него 10 случайных чисел от 0 до 50
+            int[] array = new int[10];
+            Random a = new Random();
+            string path = "Безымянный файл.txt";
+            StreamWriter sw = new StreamWriter(path);
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = a.Next(0, 50);
+                sw.Write("{0}  ", array[i]);
+            }
+            sw.Close();
+            #endregion
+            #region Извлекаем из файла числа, выносим их на коносоль, и считаем сумму
+            int S = 0;
+            StreamReader sr = new StreamReader(path);
+            string numbers = sr.ReadToEnd();
+            Console.WriteLine(numbers);
+            string[] numbersArray = numbers.Split();
+            foreach (string n in numbersArray)
+            {
+                if (n != "")
+                {
+                    int i = Convert.ToInt32(n);
+                    S += i;
+                }
+            }
+            Console.WriteLine("Сумма чисел равна {0}", S);
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/bagae/OneDrive/Документы/Study/Study";
+            string[] directory = Directory.GetDirectories(path);
+            Console.WriteLine("В корневой папке {0} находятся:", path);
+            Console.WriteLine();
+            int i = 1;
+            foreach (string d in directory)
+            {
+                Console.WriteLine("{0}. {1}, а в ней находятся:", i, d);
+                string[] directory1 = Directory.GetDirectories(d);
+                foreach (string d1 in directory1)
+                {
+                    Console.WriteLine("   {0}", d1);
+                }
+                i++;
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
